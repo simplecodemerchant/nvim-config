3d7d4254dca3b623bc22dc7cdfddf4ee8ba27133
Matthew Martin <matthew.martin@forsta.com>
2024-01-26 10:04:36 -0800

    update leader-pv command to use Oil

--- lua/mmartin/remap.lua
+++ lua/mmartin/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+--vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Allows movement of selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
