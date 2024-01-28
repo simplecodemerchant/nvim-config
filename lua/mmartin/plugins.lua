19121748447556707b8b978b4ba61a8b4d9fc629
Matthew Martin <matt92martin@gmail.com>
2024-01-27 16:11:02 -0800

    crates plugin

--- lua/mmartin/plugins.lua
+++ lua/mmartin/plugins.lua
@@ -75,7 +75,7 @@ require("lazy").setup({
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
-            "3rd/image.nvim",            -- Optional image support in preview window: See `# Preview Mode` for more information
+            "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
         }
     },
     {
@@ -87,7 +87,7 @@ require("lazy").setup({
     {
         'romgrk/barbar.nvim',
         dependencies = {
-            'lewis6991/gitsigns.nvim',   -- OPTIONAL: for git status
+            'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
             'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
         },
         init = function() vim.g.barbar_auto_setup = false end,
@@ -111,4 +111,11 @@ require("lazy").setup({
             require("telescope").load_extension("lazygit")
         end,
     },
+    {
+        'saecki/crates.nvim',
+        tag = 'stable',
+        config = function()
+            require('crates').setup()
+        end,
+    }
 })
