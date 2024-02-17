238bfa8a3fe85e1c10da5e05d99a36e0c290664f
Matthew Martin <matt92martin@gmail.com>
2024-02-17 15:15:42 -0800

    clean up

--- lua/mmartin/plugins.lua
+++ lua/mmartin/plugins.lua
@@ -36,23 +36,6 @@ require("lazy").setup({
 
     'tpope/vim-fugitive',
 
-    --{
-    --    'VonHeikemen/lsp-zero.nvim',
-    --    branch = 'v3.x',
-    --    dependencies = {
-    --        --- Uncomment these if you want to manage LSP servers from neovim
-    --        {'williamboman/mason.nvim'},
-    --        {'williamboman/mason-lspconfig.nvim'},
-
-    --        -- LSP Support
-    --        {'neovim/nvim-lspconfig'},
-    --        -- Autocompletion
-    --        {'hrsh7th/nvim-cmp'},
-    --        {'hrsh7th/cmp-nvim-lsp'},
-    --        {'L3MON4D3/LuaSnip'},
-    --    }
-    --},
-
     --- Uncomment the two plugins below if you want to manage the language servers from neovim
     --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
     { 'williamboman/mason.nvim' },
