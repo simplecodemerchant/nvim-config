1110fa9eaaab3da45fc6c3b8e87bcc71008d5dfd
Matthew Martin <matthew.martin@forsta.com>
2024-06-03 11:49:57 -0700

    changes

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,3 +1,10 @@
+require("harpoon").setup({
+    -- enable tabline with harpoon marks
+    tabline = true,
+    tabline_prefix = "   ",
+    tabline_suffix = "   ",
+})
+
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
@@ -8,3 +15,12 @@ vim.keymap.set("n", "<leader>a1", function() ui.nav_file(1) end, {noremap = true
 vim.keymap.set("n", "<leader>a2", function() ui.nav_file(2) end, {noremap = true})
 vim.keymap.set("n", "<leader>a3", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<leader>a4", function() ui.nav_file(4) end)
+vim.keymap.set("n", "<A-]>", function() ui.nav_next() end, {noremap = true})
+vim.keymap.set("n", "<A-[>", function() ui.nav_prev() end, {noremap = true})
+
+
+vim.cmd('highlight! HarpoonInactive guibg=NONE guifg=#63698c')
+vim.cmd('highlight! HarpoonActive guibg=NONE guifg=white')
+vim.cmd('highlight! HarpoonNumberActive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! HarpoonNumberInactive guibg=NONE guifg=#7aa2f7')
+vim.cmd('highlight! TabLineFill guibg=NONE guifg=white')
--- lua/mmartin/plugins.lua
+++ lua/mmartin/plugins.lua
@@ -67,7 +67,7 @@ require("lazy").setup({
         "folke/trouble.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
-    "github/copilot.vim",
+    --"github/copilot.vim",
     {
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v3.x",
@@ -84,21 +84,21 @@ require("lazy").setup({
         -- Optional dependencies
         dependencies = { "nvim-tree/nvim-web-devicons" },
     },
-    {
-        'romgrk/barbar.nvim',
-        dependencies = {
-            'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-            'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-        },
-        init = function() vim.g.barbar_auto_setup = false end,
-        opts = {
-            -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-            -- animation = true,
-            -- insert_at_start = true,
-            -- …etc.
-        },
-        version = '^1.0.0', -- optional: only update when a new 1.x version is released
-    },
+    --{
+    --    'romgrk/barbar.nvim',
+    --    dependencies = {
+    --        'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+    --        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    --    },
+    --    init = function() vim.g.barbar_auto_setup = false end,
+    --    opts = {
+    --        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+    --        -- animation = true,
+    --        -- insert_at_start = true,
+    --        -- …etc.
+    --    },
+    --    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    --},
     { "folke/neodev.nvim",         opts = {} },
     {
         "kdheepak/lazygit.nvim",
