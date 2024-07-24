9bf0eaa5dd94ed39ef00694c8311bd427becddfc
Matthew Martin <matthewmartin@PGM-MartinM.local>
2024-07-24 13:27:03 -0700

    remap and full path file names in status bar

--- lua/mmartin/remap.lua
+++ lua/mmartin/remap.lua
@@ -47,3 +47,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
+vim.keymap.set("n", "<leader>s", "*")
