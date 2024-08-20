503b2e76448c75085fe923559f69e0f54595d52a
Matthew Martin <matthew.martin@forsta.com>
2024-08-20 11:59:12 -0700

    add another keymap to go back to oil

--- lua/mmartin/remap.lua
+++ lua/mmartin/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
 --vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Allows movement of selected lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -48,3 +49,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 vim.keymap.set("n", "<leader>gg", "<cmd>LazyGit<CR>")
 vim.keymap.set("n", "<leader>s", "*")
+
