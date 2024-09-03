7ffa2f3bcbd549cede110dc3bb6a251f4a62ecec
Matthew Martin <matthew.martin@forsta.com>
2024-09-03 13:03:55 -0700

    Add back tab bar

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,64 +1,64 @@
 local M = {}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+--vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>px', builtin.resume, {
-    noremap = true,
-    silent = true,
-    desc = "Resume",
+  noremap = true,
+  silent = true,
+  desc = "Resume",
 })
 --vim.keymap.set('n', '<leader>ps', function()
 --  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 --end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-vim.keymap.set("n", "<leader><leader>", function ()
+vim.keymap.set("n", "<leader><leader>", function()
   require("telescope").extensions.smart_open.smart_open()
 end, { noremap = true, silent = true })
 
 function M.tbl_length(T)
-    local count = 0
-    for _ in pairs(T) do count = count + 1 end
-    return count
+  local count = 0
+  for _ in pairs(T) do count = count + 1 end
+  return count
 end
 
 function M.get_visual_selection()
-    -- this will exit visual mode
-    -- use 'gv' to reselect the text
-    local _, csrow, cscol, cerow, cecol
-    local mode = vim.fn.mode()
-    if mode == 'v' or mode == 'V' or mode == '' then
-        -- if we are in visual mode use the live position
-        _, csrow, cscol, _ = unpack(vim.fn.getpos("."))
-        _, cerow, cecol, _ = unpack(vim.fn.getpos("v"))
-        if mode == 'V' then
-            -- visual line doesn't provide columns
-            cscol, cecol = 0, 999
-        end
-        -- exit visual mode
-        vim.api.nvim_feedkeys(
-            vim.api.nvim_replace_termcodes("<Esc>",
-                true, false, true), 'n', true)
-    else
-        -- otherwise, use the last known visual position
-        _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
-        _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  -- this will exit visual mode
+  -- use 'gv' to reselect the text
+  local _, csrow, cscol, cerow, cecol
+  local mode = vim.fn.mode()
+  if mode == 'v' or mode == 'V' or mode == '' then
+    -- if we are in visual mode use the live position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos("."))
+    _, cerow, cecol, _ = unpack(vim.fn.getpos("v"))
+    if mode == 'V' then
+      -- visual line doesn't provide columns
+      cscol, cecol = 0, 999
     end
-    -- swap vars if needed
-    if cerow < csrow then csrow, cerow = cerow, csrow end
-    if cecol < cscol then cscol, cecol = cecol, cscol end
-    local lines = vim.fn.getline(csrow, cerow)
-    -- local n = cerow-csrow+1
-    local n = M.tbl_length(lines)
-    if n <= 0 then return '' end
-    lines[n] = string.sub(lines[n], 1, cecol)
-    lines[1] = string.sub(lines[1], cscol)
-    return table.concat(lines, "\n")
+    -- exit visual mode
+    vim.api.nvim_feedkeys(
+      vim.api.nvim_replace_termcodes("<Esc>",
+        true, false, true), 'n', true)
+  else
+    -- otherwise, use the last known visual position
+    _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
+    _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  end
+  -- swap vars if needed
+  if cerow < csrow then csrow, cerow = cerow, csrow end
+  if cecol < cscol then cscol, cecol = cecol, cscol end
+  local lines = vim.fn.getline(csrow, cerow)
+  -- local n = cerow-csrow+1
+  local n = M.tbl_length(lines)
+  if n <= 0 then return '' end
+  lines[n] = string.sub(lines[n], 1, cecol)
+  lines[1] = string.sub(lines[1], cscol)
+  return table.concat(lines, "\n")
 end
 
 vim.keymap.set('v', '<leader>ps', function()
-    local text = M.get_visual_selection()
-    builtin.live_grep({
-        default_text = text
-    })
+  local text = M.get_visual_selection()
+  builtin.live_grep({
+    default_text = text
+  })
 end, {})
--- lua/mmartin/plugins.lua
+++ lua/mmartin/plugins.lua
@@ -1,110 +1,126 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    {
-        'nvim-telescope/telescope.nvim',
-        version = '0.1.5',
-        dependencies = { { 'nvim-lua/plenary.nvim' } }
+  {
+    'nvim-telescope/telescope.nvim',
+    version = '0.1.5',
+    dependencies = { { 'nvim-lua/plenary.nvim' } }
+  },
+  {
+    "danielfalk/smart-open.nvim",
+    branch = "0.2.x",
+    config = function()
+      require("telescope").load_extension("smart_open")
+    end,
+    dependencies = {
+      "kkharji/sqlite.lua",
+      -- Only required if using match_algorithm fzf
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      { "nvim-telescope/telescope-fzy-native.nvim" },
     },
-    {
-        "danielfalk/smart-open.nvim",
-        branch = "0.2.x",
-        config = function()
-            require("telescope").load_extension("smart_open")
-        end,
-        dependencies = {
-            "kkharji/sqlite.lua",
-            -- Only required if using match_algorithm fzf
-            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-            -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
-            { "nvim-telescope/telescope-fzy-native.nvim" },
-        },
+  },
+
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+      vim.cmd.colorscheme('rose-pine')
+    end
+  },
+
+  { 'nvim-treesitter/nvim-treesitter',  build = ':TSUpdate', branch = "master" },
+
+  'nvim-treesitter/playground',
+
+  'theprimeagen/harpoon',
+
+  'mbbill/undotree',
+
+  'tpope/vim-fugitive',
+
+  { 'williamboman/mason.nvim' },
+  { 'williamboman/mason-lspconfig.nvim' },
+
+  { 'VonHeikemen/lsp-zero.nvim',        branch = 'v4.x' },
+  { 'neovim/nvim-lspconfig' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/nvim-cmp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
+  {
+    'L3MON4D3/LuaSnip',
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" }
+  },
+  "saadparwaiz1/cmp_luasnip",
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  { "nvim-tree/nvim-tree.lua" },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
     },
-
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            vim.cmd.colorscheme('rose-pine')
-        end
+  },
+  { "Bilal2453/luvit-meta",   lazy = true }, -- optional `vim.uv` typings
+  --{                                          -- optional completion source for require statements and module annotations
+  --  "hrsh7th/nvim-cmp",
+  --  opts = function(_, opts)
+  --    opts.sources = opts.sources or {}
+  --    table.insert(opts.sources, {
+  --      name = "lazydev",
+  --      group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+  --    })
+  --  end,
+  --},
+  {
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
     },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
+  },
+  { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
 
-    { 'nvim-treesitter/nvim-treesitter',  build = ':TSUpdate', branch = "master" },
-
-    'nvim-treesitter/playground',
-
-    'theprimeagen/harpoon',
-
-    'mbbill/undotree',
-
-    'tpope/vim-fugitive',
-
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-
-    { 'VonHeikemen/lsp-zero.nvim',        branch = 'v4.x' },
-    { 'neovim/nvim-lspconfig' },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/nvim-cmp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    {
-        'L3MON4D3/LuaSnip',
-        -- follow latest release.
-        version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-        -- install jsregexp (optional!).
-        build = "make install_jsregexp",
-        dependencies = { "rafamadriz/friendly-snippets" }
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
     },
-    "saadparwaiz1/cmp_luasnip",
-    {
-        "folke/trouble.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
+    init = function() vim.g.barbar_auto_setup = false end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
     },
-    { "nvim-tree/nvim-tree.lua" },
-    {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-            library = {
-                -- See the configuration section for more details
-                -- Load luvit types when the `vim.uv` word is found
-                { path = "luvit-meta/library", words = { "vim%.uv" } },
-            },
-        },
-    },
-    { "Bilal2453/luvit-meta",   lazy = true }, -- optional `vim.uv` typings
-    {                                          -- optional completion source for require statements and module annotations
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-            opts.sources = opts.sources or {}
-            table.insert(opts.sources, {
-                name = "lazydev",
-                group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-            })
-        end,
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        -- optional for floating window border decoration
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim",
-        },
-        config = function()
-            require("telescope").load_extension("lazygit")
-        end,
-    },
-    { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
 })
