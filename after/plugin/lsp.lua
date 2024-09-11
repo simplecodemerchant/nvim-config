5bc1e3c93772a05d50fd6f4ce28456331089f512
Matthew Martin <matthew.martin@forsta.com>
2024-09-11 08:54:01 -0700

    changes

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,86 +1,88 @@
 local lsp_zero = require('lsp-zero')
 
 local lsp_attach = function(client, bufnr)
-    local opts = { buffer = bufnr }
+  local opts = { buffer = bufnr }
 
-    vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+  vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end
 
 lsp_zero.extend_lspconfig({
-    sign_text = true,
-    lsp_attach = lsp_attach,
-    float_border = 'rounded',
-    capabilities = require('cmp_nvim_lsp').default_capabilities(),
+  sign_text = true,
+  lsp_attach = lsp_attach,
+  float_border = 'rounded',
+  capabilities = require('cmp_nvim_lsp').default_capabilities(),
 })
 
 lsp_zero.format_on_save({
-    format_opts = {
-        async = false,
-        timeout_ms = 10000,
-    },
-    servers = {
-        ['tsserver'] = { 'javascript', 'typescript' },
-        ['lua_ls'] = { 'lua' },
-        ['pyright'] = { 'python' },
-        ['html'] = { 'html' },
-        ['gopls'] = { 'go' },
-        ["svelte"] = { "svelte" }
-    }
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ['tsserver'] = { 'javascript', 'typescript' },
+    ['lua_ls'] = { 'lua' },
+    ['pyright'] = { 'python' },
+    ['html'] = { 'html' },
+    ['gopls'] = { 'go' },
+    ["svelte"] = { "svelte" },
+    ["terraformls"] = { "terraform", "terraform-vars" }
+  }
 })
 
 local util = require("lspconfig/util")
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    -- Replace the language servers listed here
-    -- with the ones you want to install
-    ensure_installed = {
-        'rust_analyzer',
-        'tailwindcss',
-        'gopls',
-        'tsserver',
-        'pyright',
-        'bashls',
-        'html',
-        'lua_ls',
-        'puppet',
-        'yamlls'
-    },
-    handlers = {
-        function(server_name)
-            require('lspconfig')[server_name].setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {
+    'rust_analyzer',
+    'tailwindcss',
+    'gopls',
+    'tsserver',
+    'pyright',
+    'bashls',
+    'html',
+    'lua_ls',
+    'puppet',
+    'yamlls',
+    'terraformls'
+  },
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({
 
-            })
-        end,
+      })
+    end,
 
-        lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require("lspconfig").lua_ls.setup(lua_opts)
-        end,
+    lua_ls = function()
+      local lua_opts = lsp_zero.nvim_lua_ls()
+      require("lspconfig").lua_ls.setup(lua_opts)
+    end,
 
-        rust_analyzer = function()
-            require("lspconfig").rust_analyzer.setup({
-                filetype = { "rust" },
-                root_dir = util.root_pattern("Cargo.toml"),
-                settings = {
-                    ['rust-analyzer'] = {
-                        cargo = {
-                            allFeatures = true,
-                        },
-                    },
-                },
-            })
-        end
-    },
+    rust_analyzer = function()
+      require("lspconfig").rust_analyzer.setup({
+        filetype = { "rust" },
+        root_dir = util.root_pattern("Cargo.toml"),
+        settings = {
+          ['rust-analyzer'] = {
+            cargo = {
+              allFeatures = true,
+            },
+          },
+        },
+      })
+    end
+  },
 })
 
 local cmp = require('cmp')
@@ -88,33 +90,33 @@ local cmp_action = require('lsp-zero').cmp_action()
 local cmp_format = require('lsp-zero').cmp_format({ details = true })
 
 cmp.setup({
-    preselect = 'item',
-    completion = {
-        completeopt = 'menu,menuone,noinsert'
-    },
-    sources = {
-        { name = 'path' },
-        { name = 'nvim_lsp' },
-        { name = 'luasnip', keyword_length = 2 },
-        { name = 'buffer',  keyword_length = 3 },
-    },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    snippet = {
-        expand = function(args)
-            -- You need Neovim v0.10 to use vim.snippet
-            require('luasnip').lsp_expand(args.body)
-        end,
-    },
-    formatting = cmp_format,
-    mapping = cmp.mapping.preset.insert({
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-CR>'] = cmp.mapping.confirm({ select = false }),
-        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-    }),
+  preselect = 'item',
+  completion = {
+    completeopt = 'menu,menuone,noinsert'
+  },
+  sources = {
+    { name = 'path' },
+    { name = 'nvim_lsp' },
+    { name = 'luasnip', keyword_length = 2 },
+    { name = 'buffer',  keyword_length = 3 },
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  snippet = {
+    expand = function(args)
+      -- You need Neovim v0.10 to use vim.snippet
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  formatting = cmp_format,
+  mapping = cmp.mapping.preset.insert({
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-CR>'] = cmp.mapping.confirm({ select = false }),
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }),
 })
 
 --lsp_zero.on_attach(function(_, bufnr)
--- lua/mmartin/plugins.lua
+++ lua/mmartin/plugins.lua
@@ -68,10 +68,6 @@ require("lazy").setup({
     dependencies = { "rafamadriz/friendly-snippets" }
   },
   "saadparwaiz1/cmp_luasnip",
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-  },
   { "nvim-tree/nvim-tree.lua" },
   {
     "folke/lazydev.nvim",
@@ -123,4 +119,41 @@ require("lazy").setup({
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
+  {
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  }
 })
