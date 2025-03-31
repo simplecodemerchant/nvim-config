2ea1d468c7da575a9321665789e5ea4b06f1dfc5
Matthew Martin <matthew.martin@forsta.com>
2025-03-31 11:51:28 -0700

    adjust yank to work for me

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- next greatest remap ever : asbjornHaland
+-- yank to PC clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
